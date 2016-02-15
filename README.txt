97a10d9246eeacfee9a23c14c4905a62427def22
ewhuang <eddie.a7x@gmail.com>
2016-02-15 13:37:32 -0600

    Updated readme and

--- README.txt
+++ README.txt
@@ -21,6 +21,24 @@ First, download the ZRS data.
 
 $ python preprocess_ZRS_data
 
-Outputs two files, ZRS_patient_symptoms.txt. Each symptom is sorted by a visit
+Outputs a file, ZRS_patient_symptoms.txt. Each symptom is sorted by a visit
 id number.
 
+The second file, ZRS_patient_drugs.txt, can simply be copied from the
+appropriate columns from the raw excel file.
+Next, we normalize the data.
+
+$ python normalize_ZRS_data.py
+
+It uses a similar technique to IDF to remove symptoms or drugs that appear too
+frequently.
+
+$ python mine_ZRS_frequent_pattersn.py
+
+Outputs a file, ZRS_freq_patterns.txt, which contains the frequent patterns and
+their support on each newline.
+
+$ python filter_ZRS_frequent_patterns.py
+
+Outputs norm_ZRS_freq_patterns.txt, which reformats each frequent pattern into
+a drug, symptom set relationship, along with their previous support.
--- mine_ZRS_frequent_patterns.py
+++ mine_ZRS_frequent_patterns.py
@@ -1,6 +1,6 @@
 ### Author: Edward Huang
 
-from pymining import itemmining, assocrules
+from pymining import itemmining
 import time
 
 ### This script takes the symptoms and drugs prescribed to each patient, and
@@ -17,15 +17,15 @@ if __name__ == '__main__':
 
     # Dictionary, with keys as patient ids, as values as lists of symptoms.
     symptom_dct = {}
-    symptom_visit_nos = set([])
+    # symptom_visit_nos = set([])
     # Read in the symptoms file.
-    f = open('./data/ZRS_patient_symptoms.txt', 'r')
+    f = open('./data/norm_ZRS_patient_symptoms.txt', 'r')
     for line in f:
         line = line.split()
         visit_no = line[0]
         symptoms = line[1:]
         symptom_dct[visit_no] = symptoms
-        symptom_visit_nos.add(visit_no)
+        # symptom_visit_nos.add(visit_no)
         for symptom in symptoms:
             if symptom not in symptom_drug_list:
                 symptom_drug_list += [symptom]
@@ -34,7 +34,7 @@ if __name__ == '__main__':
     drug_dct = {}
     drug_set = set([])
     # drug_visit_nos = set([])
-    f = open('./data/ZRS_patient_drugs.txt', 'r')
+    f = open('./data/norm_ZRS_patient_drugs.txt', 'r')
     for i, line in enumerate(f):
         # Skip the header.
         if i == 0:
@@ -72,39 +72,56 @@ if __name__ == '__main__':
         curr_trans = tuple(symptoms + drugs)
         transactions += [curr_trans]
 
-    transactions = tuple(transactions)
-
-    # Write out the association rules.
-    out = open('./results/ZRS_association_rules.txt', 'w')
-
     relim_input = itemmining.get_relim_input(transactions)
     item_sets = itemmining.relim(relim_input, min_support=MIN_SUP)
-    print 'Mining associations...'
-    rules = assocrules.mine_assoc_rules(item_sets, min_support=MIN_SUP, min_confidence=0.5)
-    print 'Writing out to file...'
-    for cause_set, effect_set, support, confidence in rules:
-        # For each association rule, check to see if there is at least one drug
-        # in the rule.
-        drug_in_rule = False
-        for cause in cause_set:
-            cause = symptom_drug_list[cause]
-            if cause in drug_set:
-                drug_in_rule = True
-                break
-        for effect in effect_set:
-            cause = symptom_drug_list[effect]
-            if effect in drug_set:
-                drug_in_rule = True
-                break
-        if not drug_in_rule:
+
+
+    out = open('./results/ZRS_freq_patterns.txt', 'w')
+    for item_set in item_sets:
+        if len(item_set) == 1:
             continue
-        # Write out the remaining association rules.
-        for cause in cause_set:
-            out.write(symptom_drug_list[cause] + ', ')
-        out.write('->' )
-        for effect in effect_set:
-            out.write(symptom_drug_list[effect] + ', ')
-        out.write('supp=%d, conf=%f\n' % (support, confidence))
-    out.close
+        drug_in_item_set = False
+        for item in item_set:
+            item = symptom_drug_list[item]
+            if item in drug_set:
+                drug_in_item_set = True
+                break
+        if not drug_in_item_set:
+            continue
+        for item in item_set:
+            out.write(symptom_drug_list[item] + ', ')
+        freq = item_sets[item_set]
+        out.write('%d\n' % freq)
+    out.close()
+
+    # print 'Mining associations...'
+    # mine_assoc_rules(item_sets, min_support=MIN_SUP, min_confidence=0.5)
+
+    # print 'Writing out to file...'
+    # out = open('./results/ZRS_association_rules.txt', 'w')
+    # for cause_set, effect_set, support, confidence in rules:
+    #     # For each association rule, check to see if there is at least one drug
+    #     # in the rule.
+    #     drug_in_rule = False
+    #     for cause in cause_set:
+    #         cause = symptom_drug_list[cause]
+    #         if cause in drug_set:
+    #             drug_in_rule = True
+    #             break
+    #     for effect in effect_set:
+    #         cause = symptom_drug_list[effect]
+    #         if effect in drug_set:
+    #             drug_in_rule = True
+    #             break
+    #     if not drug_in_rule:
+    #         continue
+    #     # Write out the remaining association rules.
+    #     for cause in cause_set:
+    #         out.write(symptom_drug_list[cause] + ', ')
+    #     out.write('->' )
+    #     for effect in effect_set:
+    #         out.write(symptom_drug_list[effect] + ', ')
+    #     out.write('supp=%d, conf=%f\n' % (support, confidence))
+    # out.close()
 
     print "---%f seconds---" % (time.time() - start_time)
