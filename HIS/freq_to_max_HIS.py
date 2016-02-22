696f39e23dedc4e3dc7b059f31445c9f584983e4
Edward Huang <ewhuang3@timan103.cs.illinois.edu>
2016-02-22 14:54:49 -0600

    Added freq_to_max_HIS.py

--- HIS/freq_to_max_HIS.py
+++ HIS/freq_to_max_HIS.py
@@ -14,7 +14,6 @@ from collections import OrderedDict
 def has_ele(lst, st):
     for ele in lst:
         if ele in st:
-            print ele
             return True
     return False
 
@@ -22,65 +21,23 @@ if __name__ == '__main__':
     reload(sys)
     sys.setdefaultencoding('cp1252')
 
-    # Reading in the index dictionary for drugs and symptoms.
-    drug_symptom_counts = OrderedDict({})
-    transactions = []
-    # Keeps track of all of the herbs and symptoms.
-    all_herbs = set([])
-    all_symptoms = set([])
-    f = open('./data/HIS_clean_data.txt', 'r')
+    drug_and_symptoms = []
+    f = open('./data/HIS_herb_symptom_mappings.txt', 'r')
     for i, line in enumerate(f):
-        # Skip header
-        if i == 0:
-            continue
-        line = line.replace('，',' ').replace('。',' ').replace(',', ' ')
-        line = line.replace('小', '')
-        date, symptoms, herbs = line.split('\t')
-        symptoms = symptoms.split()
-        # Each row is a unique transaction.
-        herbs_no_dosage = []
-        for herb in herbs.split():
-            # Skip the elements in the herb list that are dosages.
-            if herb[0].isdigit():
-                continue
-            herbs_no_dosage += [herb]
-        transaction = symptoms + herbs_no_dosage
-        # Count the number of times each drug or symptom appears.
-        for element in transaction:
-            if element not in drug_symptom_counts:
-                drug_symptom_counts[element] = 1
-            else:
-                drug_symptom_counts[element] += 1
-            # Check what the element was.
-            if element in herbs_no_dosage:
-                all_herbs.add(element)
-            else:
-                all_symptoms.add(element)
-        transactions += [transaction]
+        drug_and_symptoms += [line.strip()]
     f.close()
 
-    # Get rid of symptoms or drugs that do not appear many times, or appear too
-    # much.
-    num_transactions = len(transactions)
-    bad_elements = []
-    for element in drug_symptom_counts:
-        count = drug_symptom_counts[element]
-        if count > 0.1 * num_transactions or count < 5:
-            bad_elements += [element]
-    for bad_element in bad_elements:
-        del drug_symptom_counts[bad_element]
+    all_herbs = set([])
+    f = open('./data/HIS_herbs.txt', 'r')
+    for line in f:
+        all_herbs.add(drug_and_symptoms[int(line.strip())])
+    f.close()
 
-    # Delete the actual bad symptoms or drugs from each transaction.
-    for i, transaction in enumerate(transactions):
-        for element in bad_elements:
-            while element in transaction:
-                transactions[i].remove(element)
-
-    # Change the drugs and symptoms to their corresponding indices.
-    drug_and_symptoms = drug_symptom_counts.keys()
-
-    #________________________________________________
-    # Actual Max Mining.
+    all_symptoms = set([])
+    f = open('./data/HIS_symptoms.txt', 'r')
+    for line in f:
+        all_symptoms.add(drug_and_symptoms[int(line.strip())])
+    f.close()
 
     max_patterns = []
     pat_sup_dct = {}
